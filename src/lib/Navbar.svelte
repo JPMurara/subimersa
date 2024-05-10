4a3b66c4832d41ecaff675be68f11da590e75168
Joao Murara <joaopaulo.murara@gmail.com>
2024-05-11 00:16:24 +1000

    nav bar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,3 +1,14 @@
-<div class="flex justify-center">
-    <p class="text-red-500">test aaa</p>
-</div>
+<nav class="flex flex-col md:flex-row items-center justify-around py-2 bg-blue-4 bg-cover bg-center" style="background-image: url('/bg-fundo-mar.jpeg')">
+    <div class="mb-2 md:mb-0">
+        <a href="/" class="flex items-center gap-4 md:gap-8">
+        <img src=/logo.jpg alt="subimersa logo" class="w-24 rounded-full">
+        <span class="font-poetsen-one">SUBIMERSA Curso de Pesca Submarina</span>
+        </a>
+    </div>
+
+    <div class="flex gap-8 font-poetsen-one mb-1 md:mb-0">
+        <a href="/sobre">Sobre</a>
+        <a href="/cursos">Cursos</a>
+        <a href="/contato">Contato</a>
+    </div>
+</nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poetsen-one': ['Poetsen One', 'sans-serif'],
+        // Add other custom font families here
+      },
+      colors: {
+				blue: {
+					1: ' #026891',
+					2: '#afd6e6',
+					3: '#19a9d1',
+					4: '#afd6e6'
+				}
+    }
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography'),]
 }
-
+}
