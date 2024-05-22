1b5171eee5d9fc6d75358686e71fea4b216c387d
Joao Murara <joaopaulo.murara@gmail.com>
2024-05-22 21:40:49 +1000

    vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,41 +1,29 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 import tailwindcss from 'tailwindcss';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: [
 		preprocess({
-			postcss: true // This ensures PostCSS (which Tailwind is a plugin of) is enabled
+			postcss: true
 		}),
 		vitePreprocess({
 			style: {
 				postcss: {
-					plugins: [
-						tailwindcss() // Adding Tailwind CSS as a PostCSS plugin
-					]
+					plugins: [tailwindcss()]
 				}
 			}
 		})
 	],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		// Setup base path for GitHub Pages
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/subimersa' : ''
-		}
-		// Additional configurations might be required depending on your setup
+		adapter: adapter()
+		// No need to set the paths.base for Vercel unless you're doing something unusual
+		// paths: {
+		//   base: process.env.NODE_ENV === 'production' ? '/yourbasepath' : ''
+		// },
 	}
 };
 
