2bb3c18233a5d480673de6005db835d8d2637c20
Joao Murara <joaopaulo.murara@gmail.com>
2024-09-17 23:29:14 +1000

    add vercel to gitignore

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.D1jnPDxC.js","_app/immutable/chunks/scheduler.DudDZvbt.js","_app/immutable/chunks/index.BbkFrGTK.js","_app/immutable/chunks/entry.CPDyFn4u.js"];
-export const stylesheets = [];
-export const fonts = [];
