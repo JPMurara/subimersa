28889b28860173495c6bc1160220bb1f086618b8
Joao Murara <joaopaulo.murara@gmail.com>
2024-10-03 20:52:07 +1000

    hero and nav bar

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -7,37 +7,46 @@
 	}
 </script>
 
+<!-- navbar -->
 <div class="bg-cover bg-center" style="background-image: url('/bg-fundo-mar.jpeg');">
 	<header class="">
-		<nav class="flex flex-col md:flex-row items-center justify-around py-2">
+		<nav class="flex flex-col xl:flex-row items-center justify-around py-2">
 			<div class="mb-2 md:mb-0">
 				<a href="/" class="flex items-center gap-4 md:gap-8">
-					<img src="/logo-removebg.png" alt="subimersa logo" class="w-24" />
-					<p class="">Subimersa Curso de Pesca Submarina</p>
+					<img src="/logo-removebg.png" alt="subimersa logo" class="w-20 md:w-28 xl:w-36" />
+					<p class="text-3xl font-semibold hidden lg:block">
+						<span class="font-bold">Subimersa</span> | Curso de Pesca Submarina
+					</p>
+					<div class="flex flex-col items-center lg:hidden">
+						<p class="text-3xl font-bold">Subimersa</p>
+						<p class="text-xl font-semibold">Curso de Pesca Submarina</p>
+					</div>
 				</a>
 			</div>
 
 			<div class="flex gap-8 mb-1 md:mb-0">
-				<a href="/sobre" title="Sobre {constants.name_long}">Sobre</a>
-				<a href="/cursos" title="Nossos Cursos">Cursos</a>
-				<button on:click={handleClick} title="Contato via WhatsApp">Contato</button>
-				<a href={constants.online_store} title="Conheça nossos Produtos">Loja Virtual</a>
+				<a href="/sobre" class="text-lg font-semibold" title="Sobre {constants.name_long}">Sobre</a>
+				<a href="/cursos" class="text-lg font-semibold" title="Nossos Cursos">Cursos</a>
+				<button on:click={handleClick} class="text-lg font-semibold" title="Contato via WhatsApp"
+					>Contato</button
+				>
+				<a
+					href={constants.online_store}
+					class="text-lg font-semibold"
+					title="Conheça nossos Produtos">Loja Virtual</a
+				>
 			</div>
 		</nav>
 	</header>
 
 	<section class="pt-10 md:pt-0">
 		<div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-			<div
-				class="mr-auto place-self-center text-center lg:text-left lg:col-span-7 lg:pl-4 pt-0 lg:-mt-20"
-			>
-				<h1
-					class="max-w-2xl mb-8 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl"
-				>
+			<div class="place-self-center text-center lg:text-left lg:col-span-7 lg:pl-4 pt-0 lg:-mt-20">
+				<h1 class="max-w-2xl mb-8 !text-3xl lg:!text-4xl xl:!text-5xl">
 					Curso de Pesca Subaquática em Apneia
 				</h1>
-				<p class="max-w-2xl mb-6 font-semibold text-gray-600 lg:mb-10 md:text-3xl lg:text-4xl">
-					Descubra a emoção da pesca mais Radical e Sustentável conectando-se com a natureza
+				<p class="max-w-2xl mb-6 font-semibold text-gray-700 lg:mb-10 !text-xl md:!text-3xl">
+					Descubra a emoção da pesca mais radical e sustentável conectando-se com a natureza
 					subaquática de uma forma única
 				</p>
 
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -7,23 +7,33 @@
 	}
 </script>
 
-<header class="sticky top-0">
-	<nav
-		class="flex flex-col md:flex-row items-center justify-around py-2 bg-cover bg-center"
-		style="background-image: url('/bg-fundo-mar.jpeg');"
-	>
+<header
+	class="sticky top-0 bg-cover bg-center"
+	style="background-image: url('/bg-fundo-mar.jpeg');"
+>
+	<nav class="flex flex-col xl:flex-row items-center justify-around py-2">
 		<div class="mb-2 md:mb-0">
 			<a href="/" class="flex items-center gap-4 md:gap-8">
-				<img src="/logo-removebg.png" alt="subimersa logo" class="w-24" />
-				<p class="">Subimersa Curso de Pesca Submarina</p>
+				<img src="/logo-removebg.png" alt="subimersa logo" class="w-20 md:w-28 xl:w-36" />
+				<p class="text-3xl font-semibold hidden lg:block">
+					<span class="font-bold">Subimersa</span> | Curso de Pesca Submarina
+				</p>
+				<div class="flex flex-col items-center lg:hidden">
+					<p class="text-3xl font-bold">Subimersa</p>
+					<p class="text-xl font-semibold">Curso de Pesca Submarina</p>
+				</div>
 			</a>
 		</div>
 
 		<div class="flex gap-8 mb-1 md:mb-0">
-			<a href="/sobre" title="Sobre {constants.name_long}">Sobre</a>
-			<a href="/cursos" title="Nossos Cursos">Cursos</a>
-			<button on:click={handleClick} title="Contato via WhatsApp">Contato</button>
-			<a href={constants.online_store} title="Conheça nossos Produtos">Loja Virtual</a>
+			<a href="/sobre" class="text-lg font-semibold" title="Sobre {constants.name_long}">Sobre</a>
+			<a href="/cursos" class="text-lg font-semibold" title="Nossos Cursos">Cursos</a>
+			<button on:click={handleClick} class="text-lg font-semibold" title="Contato via WhatsApp"
+				>Contato</button
+			>
+			<a href={constants.online_store} class="text-lg font-semibold" title="Conheça nossos Produtos"
+				>Loja Virtual</a
+			>
 		</div>
 	</nav>
 </header>
